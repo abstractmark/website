d01220bc80c51f17805ea1f4703171536ffb94b2
kimlimjustin <kimlimjustin@gmail.com>
2021-02-15 15:19:07 +0700

    Bump up AbstractMark version to 0.1.0-beta

--- src/Docs/index.js
+++ src/Docs/index.js
@@ -1,4 +1,6 @@
-import cheatsheet from "./cheatsheet.am"
+import cheatsheet from "./cheatsheet.am";
+import getstarted from "./getstarted.am";
 // Register docs information here
-let docs = [{title: 'AbstractMark Cheatsheet', description: "AbstractMark Cheatsheet", url: "cheatsheet", code: cheatsheet}]
+let docs = [{title: 'AbstractMark Cheatsheet', description: "A quick reference to the Abstractmark syntax.", url: "cheatsheet", code: cheatsheet},
+{title: "Getting Started | AbstractMark", description: "An overview of AbstractMark, how it works, and what you can do with it.", code: getstarted}]
 export default docs;
