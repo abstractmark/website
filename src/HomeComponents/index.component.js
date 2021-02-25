572884365e41e2a4a952016bee8f33c40f059349
kimlimjustin <kimlimjustin@gmail.com>
2021-02-25 16:22:28 +0700

    Bump up version and updated docs

--- src/HomeComponents/index.component.js
+++ src/HomeComponents/index.component.js
@@ -3,10 +3,15 @@ import raw from "./index.am";
 import Axios from "axios";
 import {AbstractMark} from "@abstractmark/abstractmark";
 
+const performaceCheckerStart = Date.now()
+
 const HomePage = () => {
     const [homePage, setHomePage] = useState('');
     useEffect(() => {
         Axios.get(raw).then(res => setHomePage(AbstractMark(res.data, {styled: false})))
+        .then(() => {
+            document.querySelector("#speed").innerHTML = `${(Date.now() - performaceCheckerStart)/1000}s`
+        })
     }, [])
     return(
         <div dangerouslySetInnerHTML={{ __html: homePage }}></div>
